9ba8ecc9756169495a5894008ad9f21b55b59b15
patrickSlaine <67559364+patrickSlaine@users.noreply.github.com>
2023-10-02 21:47:46 +0100

    User Getter Unit Tests added

---
+++ src/test/java/com/Wildlife/Recording/model/UserTests.java
@@ -0,0 +1,50 @@
+package com.Wildlife.Recording.model;
+
+import com.Wildlife.Recording.model.entities.PasswordHash;
+import com.Wildlife.Recording.model.entities.User;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.UUID;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserTests {
+
+    private static String testUserName = "TestUser";
+    private static String testFirstName = "Joe";
+    private static String testLastName = "Bloggs";
+    private static Date dob = new Date("10/10/2002");
+    private static Date joinDate = new Date("10/11/2022");
+    private static PasswordHash passwordHash = new PasswordHash(
+            null,
+            "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8");
+    private User user;
+    @BeforeEach
+    private void beforeEach(){
+        user = new User(
+                null,
+                testUserName,
+                testFirstName,
+                testLastName,
+                dob,
+                joinDate,
+                passwordHash);
+    }
+
+    @Test
+    public void getterTesting(){
+        assertEquals(testUserName, user.getUserName());
+        assertEquals(testFirstName,user.getFirstName());
+        assertEquals(testLastName,user.getLastName());
+        assertEquals(dob, user.getDateOfBirth());
+        assertEquals(joinDate, user.getJoiningDate());
+        assertEquals(passwordHash,user.getPasswordHash());
+    }
+
+    @Test
+    public void setterTesting(){
+
+    }
+}
