0a3114b10c9b8158fbfc3e9ff0843e0d43f05457
patrickSlaine <67559364+patrickSlaine@users.noreply.github.com>
2023-10-05 06:29:25 +0100

    Added logs to Password & Recording Controller

--- src/main/java/com/Wildlife/Recording/controller/PasswordController.java
+++ src/main/java/com/Wildlife/Recording/controller/PasswordController.java
@@ -9,6 +9,8 @@ import com.Wildlife.Recording.model.exceptions.UserNotFoundException;
 import com.Wildlife.Recording.service.PasswordService;
 import com.Wildlife.Recording.service.UserManagementService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,34 +24,44 @@ import static org.springframework.http.HttpStatus.*;
 @RequestMapping("/api/password")
 public class PasswordController {
 
+    Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     private PasswordService passwordService;
 
     @GetMapping("/salt/{uuid}")
     public ResponseEntity<String> getSalt(@PathVariable UUID uuid){
         try{
+            logger.info("Received HTTP GET",uuid);
             String salt = passwordService.retrieveSalt(uuid);
             return ResponseEntity.ok(salt);
         }catch(PasswordHashNotFoundException exception){
+            logger.warn("User Error", exception.getMessage());
             throw new ResponseStatusException(NOT_FOUND, exception.getMessage());
         }catch(Exception exception){
-            throw new ResponseStatusException(INTERNAL_SERVER_ERROR, exception.getMessage());
+            logger.error("Internal Error", exception.getMessage());
+            throw new ResponseStatusException(INTERNAL_SERVER_ERROR, "Error while processing your request.");
         }
     }
 
     @PutMapping("/updatePassword/{userUuid}&{passwordUuid}")
     public ResponseEntity<?> updatePassword(@PathVariable("userUuid") UUID userUuid, @PathVariable("passwordUuid") UUID passwordUuid, @Valid @RequestBody PasswordUpdate passwordData){
         try{
+            logger.info("Received HTTP PUT", userUuid,passwordUuid,passwordData);
             passwordService.updatePassword(userUuid,passwordUuid,passwordData.oldPasswordHash, passwordData.newPasswordHash, passwordData.newSalt);
             return ResponseEntity.ok("User Created");
         }catch(UserNotFoundException exception){
+            logger.warn("User Error",exception.getMessage());
             throw new ResponseStatusException(NOT_FOUND, exception.getMessage());
         }catch(PasswordHashNotFoundException exception){
+            logger.warn("User Error",exception.getMessage());
             throw new ResponseStatusException(NOT_FOUND, exception.getMessage());
         }catch(InvalidPasswordException exception){
+            logger.warn("User Error",exception.getMessage());
             throw new ResponseStatusException(BAD_REQUEST, exception.getMessage());
         }
         catch(Exception exception){
+            logger.error("Internal Error",exception.getMessage());
             throw new ResponseStatusException(INTERNAL_SERVER_ERROR,exception.getMessage());
         }
     }
@@ -57,6 +69,7 @@ public class PasswordController {
     @GetMapping("/validate")
     public ResponseEntity<?> validatePassword(@Valid @RequestBody PasswordValidate passwordValidate){
         try{
+            logger.info("Received HTTP GET",passwordValidate);
             boolean validated = passwordService.validatePassword(
                                                 passwordValidate.passwordHashUuid,
                                                 passwordValidate.hashValue);
@@ -65,8 +78,10 @@ public class PasswordController {
             }
             return ResponseEntity.badRequest().build();
         }catch(PasswordHashNotFoundException exception){
+            logger.warn("User Error",exception.getMessage());
             throw new ResponseStatusException(NOT_FOUND,exception.getMessage());
         }catch(Exception exception){
+            logger.error("Internal Error", exception.getMessage());
             throw new ResponseStatusException(INTERNAL_SERVER_ERROR, exception.getMessage());
         }
     }
--- src/main/java/com/Wildlife/Recording/controller/RecordingController.java
+++ src/main/java/com/Wildlife/Recording/controller/RecordingController.java
@@ -4,6 +4,8 @@ import com.Wildlife.Recording.model.entities.Recording;
 import com.Wildlife.Recording.model.exceptions.RecordingNotFoundException;
 import com.Wildlife.Recording.service.RecordingService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,29 +21,38 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 @RequestMapping("/api/recording")
 public class RecordingController {
 
+    Logger logger = LoggerFactory.getLogger(UserController.class);
     @Autowired
     private RecordingService recordingService;
 
     @GetMapping("/get/{uuid}")
     public Recording getRecording(@PathVariable UUID uuid){
         try{
+            logger.info("Received HTTP GET",uuid);
             return recordingService.readRecording(uuid);
-        }catch(Exception exception){
+        }catch(RecordingNotFoundException exception){
+            logger.warn("User Error", exception.getMessage());
             throw new ResponseStatusException(NOT_FOUND, exception.getMessage());
+        }catch(Exception exception){
+            logger.error("Internal Error",exception.getMessage());
+            throw new ResponseStatusException(INTERNAL_SERVER_ERROR,exception.getMessage());
         }
     }
 
     @GetMapping("/get")
     public List<Recording> getRecordings(){
+        logger.info("Received HTTP GET");
         return recordingService.readAllRecording();
     }
 
     @PostMapping("/post")
     public ResponseEntity<?> postRecording(@Valid @RequestBody Recording recording){
+        logger.info("Received HTTP POST",recording);
         boolean added = recordingService.createRecording(recording);
         if(added){
             return ResponseEntity.ok("Recording Added");
         }else{
+            logger.error("Internal Error",recording);
             return ResponseEntity.internalServerError().build();
         }
     }
@@ -49,10 +60,13 @@ public class RecordingController {
     @PutMapping("/put/{uuid}")
     public ResponseEntity<?> putRecording(@PathVariable UUID uuid, @Valid @RequestBody Recording recording){
         try{
+            logger.info("Received HTTP PUT",uuid,recording);
             return ResponseEntity.ok(recordingService.updateRecording(uuid,recording));
         }catch(RecordingNotFoundException exception){
+            logger.warn("User Error",exception.getMessage());
             throw new ResponseStatusException(NOT_FOUND, exception.getMessage());
         }catch(Exception exception){
+            logger.error("Internal Error",exception.getMessage());
             throw new ResponseStatusException(INTERNAL_SERVER_ERROR, exception.getMessage());
         }
     }
@@ -60,11 +74,14 @@ public class RecordingController {
     @DeleteMapping("/delete/{uuid}")
     public ResponseEntity<?> deleteRecording(@PathVariable UUID uuid){
         try{
+            logger.info("Received HTTP DELETE", uuid);
             recordingService.deleteRecording(uuid);
             return ResponseEntity.ok().build();
         }catch(RecordingNotFoundException exception){
+            logger.warn("User Error",exception.getMessage());
             throw new ResponseStatusException(NOT_FOUND, exception.getMessage());
         }catch(Exception exception){
+            logger.error("Internal Error",exception.getMessage());
             throw new ResponseStatusException(INTERNAL_SERVER_ERROR, exception.getMessage());
         }
     }
